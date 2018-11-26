10cc8f99e0cb9674de01ebdd61140fdcd6ae8b8a
landrews <lk454752@dal.ca>
2018-11-26 17:19:23 -0400

    Implementing Base Page Views

--- app.js
+++ app.js
@@ -7,10 +7,59 @@ const app = express();
 app.set('views', './public/views');
 app.set('view engine', 'ejs');
 
-//set the defualt route
+//Default Route - Index
 app.get('/', (req, res) => {
     res.render('pages/index');
 });
+
+//HOME
+app.get('/home', (req, res) => {
+    res.render('pages/home');
+})
+
+app.get('/about', (req, res) => {
+    res.render('pages/about');
+})
+
+
+//USER LOGIN & REGISTER ROUTES
+
+// -pre login
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+})
+
+    // after submit
+app.post('/login', (req, res) => {
+    res.render('pages/login');
+})
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+})
+
+app.post('/register', (req, res) => {
+    res.render('pages/register');
+})
+
+app.get('/logout', (req, res) => {
+    res.render('pages/home');
+})
+
+
+//ORDER ROUTES
+
+app.get('/menu', (req, res) => {
+    res.render('pages/menu');
+})
+
+app.get('/basket', (req, res) => {
+    res.render('pages/basket');
+})
+
+app.get('/checkout', (req, res) => {
+    res.render('pages/checkout');
+})
 
 
 app.listen(8080);
