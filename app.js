31b8d2f7cf19d2ba7a60bad74331e50fbbe4b780
Sam Irvine <Sam.Irvine@dal.ca>
2018-11-27 13:20:56 -0400

    Added views

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express();
 //set app view engine
 app.set('views', './public/views');
 app.set('view engine', 'ejs');
+//set application public files
+app.use(express.static(__dirname + '/public'));
 
 //set the defualt route
 app.get('/', (req, res) => {
-    res.render('pages/index');
+    res.render('pages/home');
+});
+//set the defualt route
+app.get('/menu', (req, res) => {
+    res.render('pages/menu');
 });
 
 
