9e62ea882c8a8563a117426ba3c3593d2de20a3c
landrews <lk454752@dal.ca>
2018-12-06 13:39:01 -0400

    Adjusting mongodb connection

--- app.js
+++ app.js
@@ -24,136 +24,135 @@ var MongoClient = require('mongodb').MongoClient;
 var resultArray = [];
 MongoClient.connect('mongodb://localhost:27017/testDB', function (err, db) {
     if (err) throw err;
-});
-app.get('/getJapaneseFood', (req, res) => {
-    var cursor = db.collection('janpaneseFood').find();
-    cursor.forEach( function (doc, err) {
-        assert.equal(null, err);
-        resultArray.push(doc);
-    }, function () {
-        db.close();
-        res.render('pages/menu', {ejsData: resultArray});
-        resultArray = [];
+
+    app.get('/getJapaneseFood', (req, res) => {
+        var cursor = db.collection('janpaneseFood').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function () {
+            db.close();
+            res.render('pages/menu', {ejsData: resultArray});
+            resultArray = [];
+        });
     });
-});
-app.get('/getItalianFood', (req, res) => {
-    var cursor = db.collection('italianFood').find();
-    cursor.forEach( function (doc, err) {
-       assert.equal(null, err);
-       resultArray.push(doc);
-    }, function () {
-       db.close();
-       res.render('pages/menu', {ejsData: resultArray});
-       resultArray = [];
+    app.get('/getItalianFood', (req, res) => {
+        var cursor = db.collection('italianFood').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function () {
+            db.close();
+            res.render('pages/menu', {ejsData: resultArray});
+            resultArray = [];
+        });
     });
-});
 //wenlong ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
 
 //Default Route - Index
-app.get('/', (req, res) => {
-    checkSession(req);
-    res.render('pages/index');
-});
+    app.get('/', (req, res) => {
+        checkSession(req);
+        res.render('pages/index');
+    });
 //set the defualt route
-app.get('/menu', (req, res) => {
-    res.render('pages/menu', {ejsData: resultArray});
-});
+    app.get('/menu', (req, res) => {
+        res.render('pages/menu', {ejsData: resultArray});
+    });
 //ACCOUNT
-app.get('/account', (req, res) => {
-    res.render('pages/account');
-});
+    app.get('/account', (req, res) => {
+        res.render('pages/account');
+    });
 
 //HOME
-app.get('/home', (req, res) => { //home page post login
-    checkSession(req);
-    if (ssn.loggedIn == true ) {
-        res.render('pages/home', { loggedIn: ssn.loggedIn });
-    }
-    else {
-        res.render('pages/home', { loggedIn: ssn.loggedIn });
-    }
- })
+    app.get('/home', (req, res) => { //home page post login
+        checkSession(req);
+        if (ssn.loggedIn == true) {
+            res.render('pages/home', {loggedIn: ssn.loggedIn});
+        } else {
+            res.render('pages/home', {loggedIn: ssn.loggedIn});
+        }
+    })
 
-app.get('/about', (req, res) => {
-    res.render('pages/about');
-})
+    app.get('/about', (req, res) => {
+        res.render('pages/about');
+    })
 
 
 //USER LOGIN & REGISTER ROUTES
 
 // login form view
-app.get('/login', (req, res) => {
-    checkSession(req)
-    res.render('pages/login');
-})
+    app.get('/login', (req, res) => {
+        checkSession(req)
+        res.render('pages/login');
+    })
 
 
     // process form submit
-app.post('/login', (req, res) => {
-    if (req.body.username === "luke" && req.body.password === "woo") { //valid login
-      res.redirect('/home');
-      ssn.loggedIn = true;
-      ssn.username = req.body.username;
-      console.log(ssn);
+    app.post('/login', (req, res) => {
+        if (req.body.username === "luke" && req.body.password === "woo") { //valid login
+            res.redirect('/home');
+            ssn.loggedIn = true;
+            ssn.username = req.body.username;
+            console.log(ssn);
 
-      //to-do Initiate session variables
-    }
-    else { //invalid login
-        res.render('pages/login/err')
-    }
-})
+            //to-do Initiate session variables
+        } else { //invalid login
+            res.render('pages/login/err')
+        }
+    })
 
-app.get('/register', (req, res) => {
-    res.render('pages/register');
-})
+    app.get('/register', (req, res) => {
+        res.render('pages/register');
+    })
 
-app.post('/register', (req, res) => {
-    res.render('pages/register');
-})
+    app.post('/register', (req, res) => {
+        res.render('pages/register');
+    })
 
-app.get('/logout', (req, res) => {
-    ssn.loggedIn = false;
-    res.redirect('/');
-})
+    app.get('/logout', (req, res) => {
+        ssn.loggedIn = false;
+        res.redirect('/');
+    })
 
 
 //ORDER ROUTES
 
-app.get('/menu', (req, res) => {
-    res.render('pages/menu', {ejsData: resultArray});
-})
+    app.get('/menu', (req, res) => {
+        res.render('pages/menu', {ejsData: resultArray});
+    })
 
 //basket
-app.get('/basket', (req, res) => {
-    res.render('pages/basket');
-})
+    app.get('/basket', (req, res) => {
+        res.render('pages/basket');
+    })
 
-app.post('/addToBasket', (req, res) => {
-    ssn.basket = ssn.basket || [];
-    ssn.basket.push(req.body.item);
-    res.end();
-})
+    app.post('/addToBasket', (req, res) => {
+        ssn.basket = ssn.basket || [];
+        ssn.basket.push(req.body.item);
+        res.end();
+    })
 
-app.post('/removeFromBasket', (req, res) => {
-    //find the index of the item to delete
-    var index = ssn.basket.indexOf(req.body.item);
-    //if the item exists remove it
-    if( index > -1 ) {
-        ssn.basket.splice(index, 1);
-    }
-    res.end();
-})
-app.get('/requestBasket', (req, res) => {
-    if (ssn != undefined) {
-        res.writeHead(200, { 'Content-Type': 'application/json' }); 
-        res.end(JSON.stringify(ssn.basket));
-    }
-    res.end();
-})
+    app.post('/removeFromBasket', (req, res) => {
+        //find the index of the item to delete
+        var index = ssn.basket.indexOf(req.body.item);
+        //if the item exists remove it
+        if (index > -1) {
+            ssn.basket.splice(index, 1);
+        }
+        res.end();
+    })
+    app.get('/requestBasket', (req, res) => {
+        if (ssn != undefined) {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(ssn.basket));
+        }
+        res.end();
+    })
 
-app.get('/checkout', (req, res) => {
-    res.render('pages/checkout');
-})
+    app.get('/checkout', (req, res) => {
+        res.render('pages/checkout');
+    })
+});
 
 function checkSession(req) {
     if (!ssn) {
